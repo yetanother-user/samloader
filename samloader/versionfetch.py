748d345853d39b5becb2a28430b77504052053b3
Dominique Martinet <asmadeus@codewreck.org>
2023-04-29 04:43:25 +0900

    Fix error 400 on version check
    fota-cloud-dn.ospserver.net seems to block the requests user agent explicitly,
    forcing curl instead makes it work for now.

--- samloader/versionfetch.py
+++ samloader/versionfetch.py
@@ -17,10 +17,9 @@ def normalizevercode(vercode: str) -> str:
 
 def getlatestver(model: str, region: str) -> str:
     """ Get the latest firmware version code for a model and region. """
-    req_headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; ' \
-        + 'rv:102.0) Gecko/20100101 Firefox/102.0'}
-    req = requests.get("https://fota-cloud-dn.ospserver.net/firmware/" \
-        + region + "/" + model + "/version.xml", headers=req_headers)
+    req = requests.get("https://fota-cloud-dn.ospserver.net/firmware/"
+                       + region + "/" + model + "/version.xml",
+                       headers={'User-Agent': 'curl/7.87.0'})
     if req.status_code == 403:
         raise Exception("Model or region not found (403)")
     req.raise_for_status()
